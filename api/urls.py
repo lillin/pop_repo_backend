65c3522782f91a9f70585bb94c091ed91edaaebd
Lina <kleine.adi@gmail.com>
2022-03-22 11:26:18 +0200

    Add get by owner API (#1)

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from api.views import RepoPopularitySearchView
+from api.views import RepoPopularitySearchView, RepoPopularityByOwnerView
 
 urlpatterns = [
     path('repo-popularity', RepoPopularitySearchView.as_view()),
+    path('repo-popularity-by-owner', RepoPopularityByOwnerView.as_view()),
 ]
